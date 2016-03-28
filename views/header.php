50376a58ff97e4731bd7c729b39cdcc6b6dbded9
Michael Warenycia <mdwarenycia@gmail.com>
2016-03-27 20:22:33 -0700

    Uploading updates
    Updated with more configuration

---
+++ views/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<script type="text/javascript" src="js/jquery-1.12.1.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+	<script type="text/javascript">
+		
+	</script>
+</head>
+<body>
+	<div id="modalOverlay"><div id="modalContent"></div></div>
+	<div id="pageHeader">
+		<div class="widthWrapper">
+			<div id="pageHeaderLogo"></div>
+			<nav>
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Cards</a>
+						<ul>
+							<li><a href="">By Name</a></li>
+							<li><a href="">By Color</a></li>
+							<li><a href="">By Edition</a></li>
+							<li><a href="">By Cost</a></li>
+						</ul>
+					</li>
+					<li><a href="">System</a>
+						<ul>
+							<li><a href="">Versions/Updates</a></li>
+							<li><a href="">Users</a></li>
+							<li><a href="">System</a></li>
+						</ul>
+					</li>
+					<li id="accountOptions">
+						<a href="">Login or Sign Up</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
